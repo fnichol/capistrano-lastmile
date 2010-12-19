c8608164085635de97c296af2ac1650b4f98e47e
Fletcher Nichol <fnichol@nichol.ca>
2010-12-18 21:25:11 -0700

    Start implementing lastmile.rb.

--- lib/capistrano/lastmile.rb
+++ lib/capistrano/lastmile.rb
@@ -1,5 +1,12 @@
 module Capistrano
   module Lastmile
-    # Your code goes here...
+
+    def self.load(&block)
+      ::Capistrano::Configuration.instance(:must_exist).load(&block)
+    end
+
+    def self.load_named(name, &block)
+      load { load(&block) unless disabled?(name) }
+    end
   end
 end
