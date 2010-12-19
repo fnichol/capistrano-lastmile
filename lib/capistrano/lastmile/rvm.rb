b74bcb01e342dbc34b0843ad944e5233adc601a7
Fletcher Nichol <fnichol@nichol.ca>
2010-12-19 01:27:24 -0700

    Style updates.

--- lib/capistrano/lastmile/rvm.rb
+++ lib/capistrano/lastmile/rvm.rb
@@ -1,6 +1,11 @@
 Capistrano::Lastmile.load_named(:rvm) do
 
+  # =========================================================================
+  # These are default variables that will be set unless overriden.
+  # =========================================================================
+
   lm_cset(:rvm_ruby_string) { "default@#{application}" }
+
 
   require 'rvm/capistrano'
 end
