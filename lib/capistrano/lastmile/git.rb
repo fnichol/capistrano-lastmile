1cc310273f0b31d24f2495fef4092ca11caed947
Fletcher Nichol <fnichol@nichol.ca>
2010-12-21 16:18:49 -0700

    Start to refactor git tag and branch calcs.

--- lib/capistrano/lastmile/git.rb
+++ lib/capistrano/lastmile/git.rb
@@ -1,32 +1,41 @@
 Capistrano::Lastmile.load_named(:git) do
-  # =========================================================================
-  # These are the tasks that are available to help with deploying web apps,
-  # and specifically, Rails applications. You can have cap give you a summary
-  # of them with `cap -T'.
-  # =========================================================================
 
   on :load do
     # only load git tasks if that's our scm
     if exists?(:scm) && scm == :git
+
+      def all_tags
+        @all_tags ||= %x{git tag}.split("\n").concat(["HEAD"]).reverse
+      end
+
+      def all_branches
+        @all_branches ||= %x{git branch --no-color}.map { |l| l.sub(/^[ *]+/, '').chomp }
+      end
+ 
+      # ======================================================================
+      # These are the tasks that are available to help with deploying web
+      # apps, and specifically, Rails applications. You can have cap give
+      # you a summary of them with `cap -T'.
+      # =========================================================================
+
       namespace :git do
 
         desc <<-DESC
           Lists all current git tags.
         DESC
         task :tags, :roles => :app, :except => { :no_release => true } do
-          tags = %x{git tag}.split("\n").reverse
-          puts "Git tags are:"
-          puts "HEAD"
-          tags.each { |t| puts t }
+          puts "\nGit tags are:\n\n"
+          all_tags.each { |t| puts "  #{t}" }
+          puts
         end
 
         desc <<-DESC
           Lists all current git branches.
         DESC
         task :branches, :roles => :app, :except => { :no_release => true } do
-          tags = %x{git branch --no-color}.map { |l| l.sub(/^[ *]+/, '').chomp }
-          puts "Git branches are:"
-          tags.each { |t| puts t }
+          puts "\nGit branches are:\n\n"
+          all_branches.each { |t| puts "  #{t}" }
+          puts
         end
       end
     end
