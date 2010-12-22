e22894dc0733bce2a5b1fe668bd6323d6dbf87a8
Fletcher Nichol <fnichol@nichol.ca>
2010-12-21 20:51:14 -0700

    Add set :branch to git recipe.

--- lib/capistrano/lastmile/git.rb
+++ lib/capistrano/lastmile/git.rb
@@ -12,6 +12,41 @@ Capistrano::Lastmile.load_named(:git) do
         @all_branches ||= %x{git branch --no-color}.map { |l| l.sub(/^[ *]+/, '').chomp }
       end
  
+      set :branch do
+        if all_tags.size == 1
+          default_tag = all_tags[0]   # get "HEAD"
+        else
+          default_tag = all_tags[1]   # use latest tag that is not "HEAD"
+        end
+
+        if ENV['tag']
+          if (all_tags | all_branches).include?(ENV['tag'])
+           tag = ENV['tag']
+          else
+            block_warn <<-MSG.sub(/^ {14}/, '')
+              Invalid tag/branch: #{ENV['tag']}. Current tag values are:
+
+              #{all_tags.inspect}
+
+              Current branch values are:
+
+              #{branches.inspect}
+            MSG
+            abort
+          end
+        else
+          tag = default_tag
+        end
+
+        if all_tags.include?(tag)
+          inform "Using git tag: [#{tag}]"
+        else
+          inform "Using git branch: [#{tag}]"
+        end
+        tag
+      end
+
+
       # ======================================================================
       # These are the tasks that are available to help with deploying web
       # apps, and specifically, Rails applications. You can have cap give
