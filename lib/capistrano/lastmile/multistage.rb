c0a399e986733517158690fdf50debe94f322a59
Fletcher Nichol <fnichol@nichol.ca>
2010-12-22 12:37:33 -0700

    Add default_stage for multistage of "staging".

--- lib/capistrano/lastmile/multistage.rb
+++ lib/capistrano/lastmile/multistage.rb
@@ -1,11 +1,13 @@
 Capistrano::Lastmile.load_named(:multistage) do
 
   begin
-    lm_cset :stages, %w{staging production}
+    lm_cset :stages,        %w{staging production}
+    lm_cset :default_stage, "staging"
 
     require 'capistrano/ext/multistage'
   rescue LoadError
     # capistrano-ext isn't loaded so don't worry about it
     unset :stages
+    unset :default_stage
   end
 end
