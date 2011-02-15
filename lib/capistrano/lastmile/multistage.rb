1c35a7057ac64737176c911876258021ff5871e0
Fletcher Nichol <fnichol@nichol.ca>
2011-02-15 16:54:30 -0700

    Add 'stages' capistrano task for multistage.

--- lib/capistrano/lastmile/multistage.rb
+++ lib/capistrano/lastmile/multistage.rb
@@ -5,6 +5,22 @@ Capistrano::Lastmile.load_named(:multistage) do
     lm_cset :default_stage, "staging"
 
     require 'capistrano/ext/multistage'
+
+    # =========================================================================
+    # These are the tasks that are available to help with deploying web apps,
+    # and specifically, Rails applications. You can have cap give you a summary
+    # of them with `cap -T'.
+    # =========================================================================
+
+    desc <<-DESC
+      Lists all valid deployment environments.
+    DESC
+    task :stages, :roles => :app, :except => { :no_release => true } do
+      puts "\nValid stages are:\n\n"
+      fetch(:stages, []).each { |s| puts "  #{s}" }
+      puts
+    end
+
   rescue LoadError
     # capistrano-ext isn't loaded so don't worry about it
     unset :stages
