0b28f8f4334beb649720cd13051e17feb04b176d
Fletcher Nichol <fnichol@nichol.ca>
2011-05-25 21:44:58 -0600

    Update whenever recipe to use gem's capistrano support (neato).

--- lib/capistrano/lastmile/recipes/whenever.rb
+++ lib/capistrano/lastmile/recipes/whenever.rb
@@ -1,27 +1,11 @@
 Capistrano::Lastmile.load_named(:whenever) do
 
   # =========================================================================
-  # These are the tasks that are available to help with deploying web apps,
-  # and specifically, Rails applications. You can have cap give you a summary
-  # of them with `cap -T'.
+  # These are default variables that will be set unless overriden.
   # =========================================================================
 
-  namespace :cron do
+  lm_cset(:whenever_command)  { "bundle exec whenever" }
 
-    desc <<-DESC
-      Updates all crontab entries for this application.
-    DESC
-    task :update, :roles => :db, :only => { :primary => true } do
-      run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
-    end
 
-    desc <<-DESC
-      Empties all crontab entries for this application.
-    DESC
-    task :clear, :roles => :db, :only => { :primary => true } do
-      run "cd #{release_path} && bundle exec whenever --clear-crontab #{application}"
-    end
-  end
-
-  after "deploy:symlink", "cron:update"
+  require 'whenever/capistrano'
 end
