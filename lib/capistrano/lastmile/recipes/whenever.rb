cf54ee1bff5f0b0f1cd97fef8d2b84b11753d02c
Fletcher Nichol <fnichol@nichol.ca>
2011-05-26 14:44:37 -0600

    Override default :whenever_identifier to include :deploy_env.

--- lib/capistrano/lastmile/recipes/whenever.rb
+++ lib/capistrano/lastmile/recipes/whenever.rb
@@ -4,7 +4,8 @@ Capistrano::Lastmile.load_named(:whenever) do
   # These are default variables that will be set unless overriden.
   # =========================================================================
 
-  lm_cset(:whenever_command)  { "bundle exec whenever" }
+  lm_cset(:whenever_command)    { "bundle exec whenever" }
+  lm_cset(:whenever_identifier) { "#{application}_#{deploy_env}" }
 
 
   require 'whenever/capistrano'
