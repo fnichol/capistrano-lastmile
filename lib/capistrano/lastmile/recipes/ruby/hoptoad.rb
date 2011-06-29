4de5518d14f84d3b9b5a8184b00e8c6f7a9c7d22
Fletcher Nichol <fnichol@nichol.ca>
2011-06-29 15:11:53 -0600

    Make deploy:notify_hoptoad run remotely on the server vs. locally.

--- lib/capistrano/lastmile/recipes/ruby/hoptoad.rb
+++ lib/capistrano/lastmile/recipes/ruby/hoptoad.rb
@@ -1,4 +1,25 @@
 Capistrano::Lastmile.load_named(:hoptoad) do
 
   require 'hoptoad_notifier/capistrano'
+
+  ##
+  # Modified from:
+  # https://github.com/thoughtbot/hoptoad_notifier/blob/master/lib/hoptoad_notifier/capistrano.rb
+  #
+  # This patch runs the rake hoptoad:deploy command on the target server rather
+  # then running locally where the necessary configuration may not exist.
+  namespace :deploy do
+    desc "Notify Hoptoad of the deployment"
+    task :notify_hoptoad, :except => { :no_release => true } do
+      rails_env = fetch(:hoptoad_env, fetch(:rails_env, "production"))
+      local_user = ENV['USER'] || ENV['USERNAME']
+      executable = RUBY_PLATFORM.downcase.include?('mswin') ? fetch(:rake, 'rake.bat') : fetch(:rake, 'rake')
+      notify_command = "cd #{release_path} && #{executable} hoptoad:deploy RAILS_ENV=#{rails_env} TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
+      notify_command << " DRY_RUN=true" if dry_run
+      notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
+      puts "Notifying Hoptoad of Deploy (#{notify_command})"
+      run notify_command, :once => true
+      puts "Hoptoad Notification Complete."
+    end
+  end
 end
